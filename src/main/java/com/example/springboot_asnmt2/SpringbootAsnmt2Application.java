edc929a201bfbba1b8e5568da33f899773a8fa5d
Sanjana T S <sanjanats37@gmail.com>
2025-02-11 22:22:20 +0530

    initial commit

---
+++ src/main/java/com/example/springboot_asnmt2/SpringbootAsnmt2Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_asnmt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAsnmt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAsnmt2Application.class, args);
+	}
+
+}
